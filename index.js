4c5e467439a555837f1da2a4d324e18cde1ed4df
Giselle <martinezgiselle5@gmail.com>
2023-08-11 13:55:17 -0400

    added inquirer prompts

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs =require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const {generateSVG, makeShape} = require('./lib/shapes');
+const { default: Choices } = require('inquirer/lib/objects/choices');
+
+inquirer
+    .prompt([
+        {
+            type:"input",
+            name:"text",
+            message: "Please enter up to 3 letters for your logo text."
+
+        },
+        {
+            type:"input",
+            name:"textColor",
+            message: "Please enter a color for your text's color."
+
+        },
+        {
+            type:"input",
+            name:"logoShape",
+            message: "Please choose a logo shape (press enter to select).",
+            choices: ['triange','circle', 'square'],
+
+        },
+        {
+            type:"input",
+            name:"logoColor",
+            message: "Please enter a color for your logo's main color."
+
+        }, 
+
+    ])
